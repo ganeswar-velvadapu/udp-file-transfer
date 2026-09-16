c07e4dbcd9e74e6f826f96b6a6c35a69dbbc0cea
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2026-09-17 00:13:09 +0530

    feat: added acks in client and server

--- client/udp_client.cpp
+++ client/udp_client.cpp
@@ -1,178 +1,153 @@
-#include <iostream>
-#include <fstream>
+#include <chrono>
 #include <cstdint>
 #include <cstring>
-#include <chrono>
+#include <fstream>
+#include <iostream>
 
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 using namespace std;
 
-enum PacketType {
-    START = 1,
-    DATA = 2,
-    ACK = 3
-};
+enum PacketType { START = 1, DATA = 2, ACK = 3 };
 
 struct PacketHeader {
-    uint32_t transaction_id;
-    uint32_t chunk_number;
-    uint8_t packet_type;
+  uint32_t transaction_id;
+  uint32_t chunk_number;
+  uint8_t packet_type;
 };
 
 int main() {
 
-    // File to send
-    const char* filename = "testing_file.md";
+  const int CHUNK_SIZE = 10;
 
-    ifstream file(filename, ios::binary);
+  const char *filename = "testing_file.md";
 
-    if (!file) {
-        cout << "Could not open file\n";
-        return 1;
+  ifstream file(filename, ios::binary);
+
+  if (!file) {
+    cout << "Could not open file\n";
+    return 1;
+  }
+
+  int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (socket_fd < 0) {
+    cout << "Could not create socket\n";
+    return 1;
+  }
+
+  sockaddr_in server;
+  server.sin_family = AF_INET;
+  server.sin_port = htons(9000);
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  uint32_t transaction_id = static_cast<uint32_t>(
+      chrono::system_clock::now().time_since_epoch().count());
+
+  // send initial start packet
+
+  PacketHeader start_header;
+
+  start_header.transaction_id = htonl(transaction_id);
+  start_header.chunk_number = htonl(0);
+  start_header.packet_type = START;
+
+  char start_packet[sizeof(PacketHeader) + 256];
+
+  memcpy(start_packet, &start_header, sizeof(PacketHeader));
+
+  int filename_size = strlen(filename);
+
+  memcpy(start_packet + sizeof(PacketHeader), filename, filename_size);
+
+  int start_packet_size = sizeof(PacketHeader) + filename_size;
+
+  sendto(socket_fd, start_packet, start_packet_size, 0, (sockaddr *)&server,
+         sizeof(server));
+
+  cout << "Sent START | "
+       << "Transaction ID: " << transaction_id << " | Filename: " << filename
+       << "\n";
+
+  // start sending data packets
+
+  char buffer[CHUNK_SIZE];
+
+  uint32_t chunk_number = 0;
+
+  while (file.read(buffer, CHUNK_SIZE) || file.gcount() > 0) {
+
+    int bytes_read = file.gcount();
+
+    PacketHeader header;
+
+    header.transaction_id = htonl(transaction_id);
+    header.chunk_number = htonl(chunk_number);
+    header.packet_type = DATA;
+
+    char packet[sizeof(PacketHeader) + CHUNK_SIZE];
+
+    memcpy(packet, &header, sizeof(PacketHeader));
+
+    memcpy(packet + sizeof(PacketHeader), buffer, bytes_read);
+
+    int packet_size = sizeof(PacketHeader) + bytes_read;
+
+    // send data
+
+    sendto(socket_fd, packet, packet_size, 0, (sockaddr *)&server,
+           sizeof(server));
+
+    cout << "Sent DATA #" << chunk_number << " | " << bytes_read << " bytes\n";
+
+    char ack_packet[sizeof(PacketHeader)];
+
+    sockaddr_in ack_server;
+    socklen_t server_size = sizeof(ack_server);
+
+    int ack_bytes = recvfrom(socket_fd, ack_packet, sizeof(ack_packet), 0,
+                             (sockaddr *)&ack_server, &server_size);
+
+    if (ack_bytes < 0) {
+      cout << "Error receiving ACK\n";
+      close(socket_fd);
+      return 1;
     }
 
-    const int CHUNK_SIZE = 10;
-    char buffer[CHUNK_SIZE];
+    // read acks 
+    
+    PacketHeader ack_header;
 
-    // Generate a transaction ID
-    uint32_t transaction_id =
-        static_cast<uint32_t>(
-            chrono::system_clock::now()
-                .time_since_epoch()
-                .count()
-        );
+    memcpy(&ack_header, ack_packet, sizeof(PacketHeader));
 
-    // UDP socket
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    uint32_t ack_transaction_id = ntohl(ack_header.transaction_id);
 
-    if (socket_fd < 0) {
-        cout << "Could not create socket\n";
-        return 1;
+    uint32_t ack_chunk_number = ntohl(ack_header.chunk_number);
+
+    // check acks
+
+    if (ack_header.packet_type == ACK && ack_transaction_id == transaction_id &&
+        ack_chunk_number == chunk_number) {
+
+      cout << "Received ACK #" << ack_chunk_number << "\n";
+
+    } else {
+
+      cout << "Invalid ACK\n";
+
+      close(socket_fd);
+      return 1;
     }
 
-    // Server address
-    sockaddr_in server;
+    chunk_number++;
+  }
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9000);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  file.close();
+  close(socket_fd);
 
-    /*
-        ------------------------------------------------
-        Send START packet
-        ------------------------------------------------
-    */
+  cout << "File transfer completed\n";
 
-    PacketHeader start_header;
-
-    start_header.transaction_id = htonl(transaction_id);
-    start_header.chunk_number = htonl(0);
-    start_header.packet_type = START;
-
-    char start_packet[
-        sizeof(PacketHeader) + 256
-    ];
-
-    memcpy(
-        start_packet,
-        &start_header,
-        sizeof(PacketHeader)
-    );
-
-    int filename_size = strlen(filename);
-
-    memcpy(
-        start_packet + sizeof(PacketHeader),
-        filename,
-        filename_size
-    );
-
-    int start_packet_size =
-        sizeof(PacketHeader) + filename_size;
-
-    sendto(
-        socket_fd,
-        start_packet,
-        start_packet_size,
-        0,
-        (sockaddr*)&server,
-        sizeof(server)
-    );
-
-    cout << "Started transfer\n";
-    cout << "Transaction ID: "
-         << transaction_id << "\n";
-    cout << "Filename: "
-         << filename << "\n";
-
-
-    /*
-        ------------------------------------------------
-        Send DATA packets
-        ------------------------------------------------
-    */
-
-    uint32_t chunk_number = 0;
-
-    while (file.read(buffer, CHUNK_SIZE) ||
-           file.gcount() > 0) {
-
-        int bytes_read = file.gcount();
-
-        cout << "Sending chunk "
-             << chunk_number
-             << " : "
-             << bytes_read
-             << " bytes\n";
-
-        PacketHeader header;
-
-        header.transaction_id =
-            htonl(transaction_id);
-
-        header.chunk_number =
-            htonl(chunk_number);
-
-        header.packet_type = DATA;
-
-        char packet[
-            sizeof(PacketHeader) + CHUNK_SIZE
-        ];
-
-        memcpy(
-            packet,
-            &header,
-            sizeof(PacketHeader)
-        );
-
-        memcpy(
-            packet + sizeof(PacketHeader),
-            buffer,
-            bytes_read
-        );
-
-        int packet_size =
-            sizeof(PacketHeader) + bytes_read;
-
-        sendto(
-            socket_fd,
-            packet,
-            packet_size,
-            0,
-            (sockaddr*)&server,
-            sizeof(server)
-        );
-
-        chunk_number++;
-    }
-
-    file.close();
-    close(socket_fd);
-
-    cout << "File transfer finished\n";
-
-    return 0;
+  return 0;
 }
--- server/udp_server.cpp
+++ server/udp_server.cpp
@@ -23,14 +23,12 @@ struct PacketHeader {
     uint8_t packet_type;
 };
 
+struct Transfer {
+    string filename;
+};
+
 int main() {
 
-    // Map:
-    // transaction ID -> filename
-
-    map<uint32_t, string> transfers;
-
-    // UDP socket
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (socket_fd < 0) {
@@ -38,36 +36,42 @@ int main() {
         return 1;
     }
 
-    // Server address
+
+    
+
     sockaddr_in server;
 
     server.sin_family = AF_INET;
     server.sin_port = htons(9000);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // Bind
-    if (bind(
-            socket_fd,
-            (sockaddr*)&server,
-            sizeof(server)) < 0) {
 
-        cout << "Could not bind socket\n";
+   
+
+    if (bind(socket_fd,(sockaddr*)&server,sizeof(server)) < 0) {
+
+        cout << "Bind failed\n";
+
         close(socket_fd);
         return 1;
     }
 
-    cout << "Server listening on port 9000...\n";
+    cout << "Server listening on port 9000\n";
 
+    map<uint32_t, Transfer> transfers;
+
+
+   // receive packets
 
     while (true) {
 
-        char packet[
-            sizeof(PacketHeader) + 256
-        ];
+        char packet[sizeof(PacketHeader) + 256];
 
         sockaddr_in client;
+
         socklen_t client_size =
             sizeof(client);
+
 
         int bytes_received = recvfrom(
             socket_fd,
@@ -79,16 +83,11 @@ int main() {
         );
 
         if (bytes_received < 0) {
+
             cout << "Error receiving packet\n";
-            break;
+            continue;
         }
-
-
-        /*
-            ----------------------------------------
-            Extract header
-            ----------------------------------------
-        */
+        // read header
 
         PacketHeader header;
 
@@ -98,39 +97,30 @@ int main() {
             sizeof(PacketHeader)
         );
 
-        uint32_t transaction_id =
-            ntohl(header.transaction_id);
+        uint32_t transaction_id = ntohl(header.transaction_id);
 
-        uint32_t chunk_number =
-            ntohl(header.chunk_number);
+        uint32_t chunk_number = ntohl(header.chunk_number);
 
-
-        /*
-            ----------------------------------------
-            START packet
-            ----------------------------------------
-        */
+        // start packet
 
         if (header.packet_type == START) {
 
             int filename_size =
-                bytes_received -
-                sizeof(PacketHeader);
+                bytes_received - sizeof(PacketHeader);
 
             string filename(
                 packet + sizeof(PacketHeader),
                 filename_size
             );
 
-            transfers[transaction_id] =
+            transfers[transaction_id].filename =
                 filename;
 
-            cout << "\nNew transfer\n";
-
+            cout << "\n";
+            cout << "Received START\n";
             cout << "Transaction ID: "
                  << transaction_id
                  << "\n";
-
             cout << "Filename: "
                  << filename
                  << "\n";
@@ -138,60 +128,39 @@ int main() {
             continue;
         }
 
-
-        /*
-            ----------------------------------------
-            DATA packet
-            ----------------------------------------
-        */
-
+        // data packets
         if (header.packet_type == DATA) {
 
-            // Check whether this transaction exists
 
-            if (transfers.find(transaction_id)
-                == transfers.end()) {
+            if (
+                transfers.find(transaction_id)
+                == transfers.end()
+            ) {
 
-                cout << "Unknown transaction ID: "
-                     << transaction_id
-                     << "\n";
-
+                cout << "Unknown transaction ID\n";
                 continue;
             }
 
-            string filename =
-                transfers[transaction_id];
 
             int data_size =
-                bytes_received -
-                sizeof(PacketHeader);
+                bytes_received - sizeof(PacketHeader);
 
-            cout << "Received chunk "
+
+            string filename =
+                transfers[transaction_id].filename;
+
+
+            cout << "Received DATA #"
                  << chunk_number
-                 << " | Transaction ID: "
-                 << transaction_id
                  << " | "
                  << data_size
                  << " bytes\n";
 
 
-            /*
-                For now we simply append the
-                received data.
-
-                Later we will replace this with
-                chunk buffering for Selective Repeat.
-            */
-
             ofstream output_file(
                 filename,
                 ios::binary | ios::app
             );
-
-            if (!output_file) {
-                cout << "Could not open output file\n";
-                continue;
-            }
 
             output_file.write(
                 packet + sizeof(PacketHeader),
@@ -199,10 +168,36 @@ int main() {
             );
 
             output_file.close();
+            
+            PacketHeader ack_header;
+
+            ack_header.transaction_id =
+                htonl(transaction_id);
+
+            ack_header.chunk_number =
+                htonl(chunk_number);
+
+            ack_header.packet_type = ACK;
+
+
+            sendto(
+                socket_fd,
+                &ack_header,
+                sizeof(PacketHeader),
+                0,
+                (sockaddr*)&client,
+                client_size
+            );
+
+
+            cout << "Sent ACK #"
+                 << chunk_number
+                 << "\n";
 
             continue;
         }
     }
+
 
     close(socket_fd);
 
